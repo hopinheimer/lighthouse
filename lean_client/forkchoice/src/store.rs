6c528a0d18f76a22754d225fb5a9bdd4ebb308a4
Hopinheimer <knmanas6@gmail.com>
2025-11-13 15:10:25 -0500

    changes

--- lean_client/forkchoice/src/store.rs
+++ lean_client/forkchoice/src/store.rs
@@ -1,4 +1,4 @@
-use lean_consensus::attestation::Attestation;
+use lean_consensus::attestation::SignedAttestation;
 use lean_consensus::attestation::Checkpoint;
 use lean_consensus::lean_block::LeanBlock;
 use lean_consensus::lean_state::LeanState;
@@ -9,8 +9,8 @@ pub struct LeanStore<E: EthSpec> {
     blocks: HashMap<Hash256, LeanBlock<E>>,
     block_states: HashMap<Hash256, LeanState<E>>,
 
-    latest_know_attestations: HashMap<u64, Attestation>,
-    latest_new_attestations: HashMap<u64, Attestation>,
+    latest_know_attestations: HashMap<u64, SignedAttestation>,
+    latest_new_attestations: HashMap<u64, SignedAttestation>,
 
     justified_checkpoint: Checkpoint,
     finalized_checkpoint: Checkpoint,
